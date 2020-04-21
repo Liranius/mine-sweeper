5da37490b9271a657c6c8d68d0351b59bcb08007
Liranius <prince.lich@gmail.com>
2020-04-21 22:02:36 +0800

    Add Core Module.

---
+++ clients/ng/projects/mine-sweeper/src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+
+import { SharedModule } from '../../../../../shared/shared.module';
+
+@NgModule({
+  imports: [SharedModule]
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule?: CoreModule) {
+    if (parentModule) {
+      throw new Error('[Core Module] CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
