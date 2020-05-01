8b694700f254a530ecebf54fb9fb3b29226d9d40
Liranius <prince.lich@gmail.com>
2020-05-01 23:04:41 +0800

    Add some class and type definitions.

---
+++ clients/ng/projects/mine-sweeper/src/app/mine-sweeper-shared/models/cell.ts
@@ -0,0 +1,81 @@
+import { Point2d } from '../../../../../../shared/models/point-2d';
+
+import { Mine, MineState } from './mine';
+
+export enum CellState {
+  Unrevealed,
+  Marked,
+  Flagged,
+  Revealed
+}
+
+export class Cell {
+  get mine(): Mine | undefined {
+    return this._mine;
+  }
+
+  get state(): CellState {
+    return this._state;
+  }
+
+  set state(value: CellState) {
+    switch (this._state) {
+      case CellState.Unrevealed:
+        if (value === CellState.Marked) {
+          throw new Error('[Cell] Only flagged cell can be marked.');
+        } else {
+          this._state = value;
+          break;
+        }
+      case CellState.Flagged:
+        if (value === CellState.Flagged || value === CellState.Marked) {
+          this._state = value;
+          break;
+        } else {
+          throw new Error('[Cell] Flagged cell can only be marked.');
+        }
+      case CellState.Marked:
+        if (value === CellState.Flagged) {
+          throw new Error('[Cell] Marked cell can not be flagged.');
+        } else {
+          this._state = value;
+          break;
+        }
+      case CellState.Revealed:
+        if (value !== CellState.Unrevealed) {
+          throw new Error('[Cell] This cell has been revealed. No way back.');
+        } else {
+          break;
+        }
+      default:
+        throw new Error(`[Cell] Unknown cell state "${value}".`);
+    }
+
+    if (this._mine) {
+      switch (this._state) {
+        case CellState.Unrevealed:
+        case CellState.Marked:
+          this._mine.state = MineState.Unrevealed;
+          break;
+        case CellState.Flagged:
+          this._mine.state = MineState.Flagged;
+          break;
+        case CellState.Revealed:
+          this._mine.state = MineState.Exploded;
+      }
+    }
+  }
+
+  private _mine?: Mine;
+  private _state: CellState = CellState.Unrevealed;
+
+  constructor(public readonly position: Point2d) {}
+
+  addMine(mine: Mine): void {
+    if (!this._mine) {
+      this._mine = mine;
+    } else {
+      throw new Error('[Cell] A mine has been added to this cell.');
+    }
+  }
+}
---
+++ clients/ng/shared/models/point-2d.ts
@@ -0,0 +1,3 @@
+export class Point2d {
+  constructor(public readonly x: number, public readonly y: number) {}
+}
