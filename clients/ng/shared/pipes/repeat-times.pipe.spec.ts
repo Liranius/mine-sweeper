63004f3cb0c6abcd47595f23eac372e7495edd75
Liranius <prince.lich@gmail.com>
2020-05-11 14:52:54 +0800

    Add repeat-times pipe.

---
+++ clients/ng/shared/pipes/repeat-times.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RepeatTimesPipe } from './repeat-times.pipe';
+
+describe('RepeatTimesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new RepeatTimesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
