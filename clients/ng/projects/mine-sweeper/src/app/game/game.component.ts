05a0b60ea793c307a3bcebe3fc064aca50e4d1fd
Liranius <prince.lich@gmail.com>
2020-06-08 21:47:02 +0800

    Apply revealCell method with GameService.

--- clients/ng/projects/mine-sweeper/src/app/game/game.component.ts
+++ clients/ng/projects/mine-sweeper/src/app/game/game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { GameConfig } from '../../../../../shared/models/game';
+import { GameService } from '../core/services/game.service';
 
 @Component({
   selector: 'mswp-game',
@@ -11,10 +12,12 @@ export class GameComponent implements OnInit {
   gameConfig?: GameConfig;
   position: 'home' | 'game' = 'home';
 
+  constructor(private readonly gameService: GameService) {}
+
   ngOnInit(): void {}
 
   startGameWith(config: GameConfig): void {
     this.position = 'game';
-    this.gameConfig = config;
+    this.gameService.start(config).subscribe(acceptedConfig => (this.gameConfig = acceptedConfig));
   }
 }
